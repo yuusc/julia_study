ec3a88bef79d9508d27aa9697ab56a8d176def85
yuusc <yuu.scorpion@gmail.com>
2020-11-01 22:13:10 +0900

    pendulum completed

--- pendulum.jl
+++ pendulum.jl
@@ -7,99 +7,30 @@ B = 0.000027
 C = 0.000027
 l = 0.8
 u = 0
-the = 0.3 #静止したい角度
+G1 = 2.0
+r = 0.0
 J = m * l * l / 3
 a0 = J * (m + M) + M * m * l * l
-x0 = [0,0,0.1,0]
+x0 = [0,0,0,0]
 
 A = [0 0 1 0;0 0 0 1;0 -m * m * l * l * g / a0 -B * (J + m * l * l) / a0 a34 = C * m * l / a0;0 (m + M) * m * l * g / a0 B * m * l / a0 -C * (m + M) / a0]
 B = [0;0;(J + m * l * l) / a0;-m * l / a0]
-println(A)
-println(B)
-f(x,p,t) = A * x + B * u
-tspan = (0.0, 1.0)
-prob = ODEProblem(f, x0, tspan)
-sol = solve(prob)
-println(sol)
-plot(sol,xlims=(0, 0.1),ylims=(the - 2.0, the + 2.0))   #最初の描画
-
-
-if float(sol[2,2]) > the
-    u=10    #最初の計算で角度が静止角度よりも大きかったらuを正に
-elseif float(sol[2,2]) < the
-    u=-10
-end
-
-max = 100   #forでの繰り返し最大回数
-for i in 1:max
-    println(i)
-    println(float(sol[1,2]))
-    x0 = [float(sol[1,2]),float(sol[2,2]),float(sol[3,2]),float(sol[4,2])]  #x0の値を計算結果より再設定
-    if float(sol[2,2]) > the && float(sol[4,2]) > 0     #角度が静止角度よりも大きく，角速度が正だったら
-        if float(sol[2,2]) > pi/3
-            println("theta" * string(float(sol[2,2])))
-            u = u + abs(float(sol[1,2]) - the) * 60
-            println(u)
-            f(x, p, t) = A * x + B * u
-            tspan = (0.0, 1.0)
-            prob = ODEProblem(f, x0, tspan)
-            sol = solve(prob)
-            println("aem")
-        else
-            println("theta" * string(float(sol[2,2])))
-            u = u + abs(float(sol[1,2]) - the) * 25
-            println(u)
-            f(x, p, t) = A * x + B * u
-            tspan = (0.0, 1.0)
-            prob = ODEProblem(f, x0, tspan)
-            sol = solve(prob)
-            println("a")
-        end
-    elseif float(sol[2,2]) > the && float(sol[4,2]) < 0
-        println("theta" * string(float(sol[2,2])))
-        u = u - abs(float(sol[1,2]) - the) * 5
-        println(u)
-        f(x, p, t) = A * x + B * u
-        tspan = (0.0, 1.0)
-        prob = ODEProblem(f, x0, tspan)
-        sol = solve(prob)
-        println("b")
-    elseif float(sol[2,2]) < the && float(sol[4,2]) > 0
-        println("theta" * string(float(sol[2,2])))
-        u = u + abs(float(sol[1,2]) - the) * 5
-        println(u)
-        f(x, p, t) = A * x + B * u
-        tspan = (0.0, 1.0)
-        prob = ODEProblem(f, x0, tspan)
-        sol = solve(prob)
-        println("c")
-    elseif float(sol[2,2]) < the && float(sol[4,2]) < 0
-        if float(sol[2,2]) < -pi/3
-            println("theta" * string(float(sol[2,2])))
-            u = u - abs(float(sol[1,2]) - the) * 60
-            println(u)
-            f(x, p, t) = A * x + B * u
-            tspan = (0.0, 1.0)
-            prob = ODEProblem(f, x0, tspan)
-            sol = solve(prob)
-            println("dem")
-        else
-            println("theta" * string(float(sol[2,2])))
-            u = u - abs(float(sol[1,2]) - the) * 25
-            println(u)
-            f(x, p, t) = A * x + B * u
-            tspan = (0.0, 1.0)
-            prob = ODEProblem(f, x0, tspan)
-            sol = solve(prob)
-            println("d")
-        end
-    end
-    if i != max
-        plot!(sol, xlims=(0, 0.1), ylims=(the - 2.0, the + 2.0))
-    end
-    if float(sol[2,2]) < -pi/2 || float(sol[2,2]) > pi/2
-        println("fall down"*string(i))
-        break
+println("A: "*string(A))
+println("B: "*string(B))
+for i in 1:100
+    println("i: "*string(i))
+    f(x,p,t) = A * x + B * u
+    tspan = (0.0, 1.0)
+    prob = ODEProblem(f, x0, tspan)
+    sol = solve(prob)
+    println("sol: "*string(sol))
+    println("x: "*string(float(sol[1,6])))
+    x = [float(sol[1,6]),float(sol[2,6]),float(sol[3,6]),float(sol[4,6])]
+    u = G1 * (r - float(sol[1,6]))
+    println("theta: "*string(float(sol[2,6])))
+    println("u: "*string(u))
+    if float(sol[2,6])<-pi/2 || float(sol[2,6]) > pi/2
+        println("fall down")
+        exit
     end
 end
-plot!(sol,xlims=(0, 0.1),ylims=(the - 2.0, the + 2.0))
--- test.jl
+++ test.jl
@@ -1,49 +1,105 @@
 using DifferentialEquations, Plots
 
-y = 1.0
-x0 = 1.0
-f(x,p,t) = 2.0x + 5.0y
+g = 9.8
+m = 1
+M = 1
+B = 0.000027
+C = 0.000027
+l = 0.8
+u = 0
+the = 0.3 #静止したい角度
+J = m * l * l / 3
+a0 = J * (m + M) + M * m * l * l
+x0 = [0,0,0.1,0]
+
+A = [0 0 1 0;0 0 0 1;0 -m * m * l * l * g / a0 -B * (J + m * l * l) / a0 a34 = C * m * l / a0;0 (m + M) * m * l * g / a0 B * m * l / a0 -C * (m + M) / a0]
+B = [0;0;(J + m * l * l) / a0;-m * l / a0]
+println(A)
+println(B)
+f(x,p,t) = A * x + B * u
 tspan = (0.0, 1.0)
 prob = ODEProblem(f, x0, tspan)
 sol = solve(prob)
 println(sol)
-plot(sol,xlims=(0, 2.0))
+plot(sol,xlims=(0, 0.1),ylims=(the - 2.0, the + 2.0))   #最初の描画
 
-max = 10
 
+if float(sol[2,2]) > the
+    u=10    #最初の計算で角度が静止角度よりも大きかったらuを正に
+elseif float(sol[2,2]) < the
+    u=-10
+end
+
+max = 100   #forでの繰り返し最大回数
 for i in 1:max
-    num = length(sol)
-    if sol[1,num] > 0
-        if sol[1,num] > 1
-            y=y-sol[1,num]/2
+    println(i)
+    println(float(sol[1,2]))
+    x0 = [float(sol[1,2]),float(sol[2,2]),float(sol[3,2]),float(sol[4,2])]  #x0の値を計算結果より再設定
+    if float(sol[2,2]) > the && float(sol[4,2]) > 0     #角度が静止角度よりも大きく，角速度が正だったら
+        if float(sol[2,2]) > pi/3
+            println("theta" * string(float(sol[2,2])))
+            u = u + abs(float(sol[1,2]) - the) * 60
+            println(u)
+            f(x, p, t) = A * x + B * u
+            tspan = (0.0, 1.0)
+            prob = ODEProblem(f, x0, tspan)
+            sol = solve(prob)
+            println("aem")
         else
-        y=y-sol[1,num]/5
+            println("theta" * string(float(sol[2,2])))
+            u = u + abs(float(sol[1,2]) - the) * 25
+            println(u)
+            f(x, p, t) = A * x + B * u
+            tspan = (0.0, 1.0)
+            prob = ODEProblem(f, x0, tspan)
+            sol = solve(prob)
+            println("a")
         end
-        f(x, p, t) = 2.0x + 5.0y
-        tspan = (0.0, 2.0)
+    elseif float(sol[2,2]) > the && float(sol[4,2]) < 0
+        println("theta" * string(float(sol[2,2])))
+        u = u - abs(float(sol[1,2]) - the) * 5
+        println(u)
+        f(x, p, t) = A * x + B * u
+        tspan = (0.0, 1.0)
         prob = ODEProblem(f, x0, tspan)
         sol = solve(prob)
-        println(sol)
-        println(y)
-        println(sol[1,num])
-        println("a")
-    elseif sol[1,num] < 0
-        if sol[1,num] < 1
-            y=y-sol[1,num]/2
-        else
-        y=y-sol[1,num]/5
-        end
-        f(x,p,t) = 2.0x + 5.0y
-        tspan = (0.0, 2.0)
-        prob = ODEProblem(f, x0, tspan)
-        sol = solve(prob)
-        println(sol)
-        println(y)
-        println(sol[1,num])
         println("b")
+    elseif float(sol[2,2]) < the && float(sol[4,2]) > 0
+        println("theta" * string(float(sol[2,2])))
+        u = u + abs(float(sol[1,2]) - the) * 5
+        println(u)
+        f(x, p, t) = A * x + B * u
+        tspan = (0.0, 1.0)
+        prob = ODEProblem(f, x0, tspan)
+        sol = solve(prob)
+        println("c")
+    elseif float(sol[2,2]) < the && float(sol[4,2]) < 0
+        if float(sol[2,2]) < -pi/3
+            println("theta" * string(float(sol[2,2])))
+            u = u - abs(float(sol[1,2]) - the) * 60
+            println(u)
+            f(x, p, t) = A * x + B * u
+            tspan = (0.0, 1.0)
+            prob = ODEProblem(f, x0, tspan)
+            sol = solve(prob)
+            println("dem")
+        else
+            println("theta" * string(float(sol[2,2])))
+            u = u - abs(float(sol[1,2]) - the) * 25
+            println(u)
+            f(x, p, t) = A * x + B * u
+            tspan = (0.0, 1.0)
+            prob = ODEProblem(f, x0, tspan)
+            sol = solve(prob)
+            println("d")
+        end
     end
     if i != max
-        plot!(sol,xlims=(0, 2.0))
+        plot!(sol, xlims=(0, 0.1), ylims=(the - 2.0, the + 2.0))
+    end
+    if float(sol[2,2]) < -pi/2 || float(sol[2,2]) > pi/2
+        println("fall down"*string(i))
+        break
     end
 end
-plot!(sol,xlims=(0, 2.0))
+plot!(sol,xlims=(0, 0.1),ylims=(the - 2.0, the + 2.0))
