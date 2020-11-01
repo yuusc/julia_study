d26831e64be7c5a23782853d23ddc31d389089fb
yuusc <yuu.scorpion@gmail.com>
2020-11-01 13:16:21 +0900

    ageを変更

--- age.jl
+++ age.jl
@@ -1,12 +1,12 @@
 #課題2
-max  = 131
-min  = 0
+max  = 131  #右側
+min  = 0    #左側
 function age(ans)
     while max - min > 1
-        mid = round(min + (max - min) / 2)
+        mid = round(min + (max - min) / 2)　#左側と右側を取り，中央の値を取得，小数を丸める
         print(mid)
         print("歳以上ですか？(yes/no)")
-        ans = chomp(readline())
+        ans = chomp(readline())　#文字列を取得( readline() )，最後の文字を削除（改行を削除）( chomp )
         if ans == "yes"
             global min = mid
         else
@@ -15,5 +15,4 @@ function age(ans)
     end
     print(min)
     print("歳です")
-    # "歳より上ですか？(yes/no)"
 end
