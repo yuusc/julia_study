8e70394a9011a198d4e511d4f659a3b798eb0bd4
yuusc <yuu.scorpion@gmail.com>
2021-01-11 17:19:57 +0900

    gain.jl

--- gain.jl
+++ gain.jl
@@ -10,8 +10,8 @@ the = pi / 2
 Tu = 0.0
 theta0 = pi / 2
 ddtheta0 = (Tu - l * M * g * sin(pi/2)) / I
-thearray = [v0] # グラフ描画用　角度の配列
-ddthearray = [u0] # グラフ描画用　各加速度の配列
+thearray = [theta0] # グラフ描画用　角度の配列
+ddthearray = [ddtheta0] # グラフ描画用　各加速度の配列
 #preP = the - pi/2
 #I = 0.0
 #D = 0.0
@@ -34,9 +34,9 @@ for i in 1:100
     sol = solve(prob, save_everystep=false) # 状態方程式を解く
     println("sol: " * string(sol))
     theta=sol[2,1]
-    println("theta: "*string(v))
-    ddtheta = (Tu- l * M * g * sin(v))/I
-    println("ddtheta: "*string(u))
+    println("theta: "*string(theta))
+    ddtheta = (Tu- l * M * g * sin(theta))/I
+    println("ddtheta: "*string(ddtheta))
     push!(thearray,theta) # 角度を配列に追加
     push!(ddthearray,ddtheta)
 end
